78215a34f7785e983d3f23b73aa2ccd7b36ce45d
Adrián <adrian.garcia8t@gmail.com>
2023-09-27 19:01:56 +0200

    update

---
+++ partA/exercise2_openloop.lua
@@ -0,0 +1,82 @@
+init = function()
+    state = {}; state.next = "WAIT"
+    I = {}; I[1] = 0; I[2] = 0; I[3] = 0 -- Initialize Instruction table
+    T = 0 -- Time
+    V = 0; W = 0 -- Linear and rotational speeds
+    L = 0; R = 0 -- Control values for left and right motors
+    M = nil -- Reply message
+end
+
+forward = function()
+    state.curr = state.next
+    if state.curr == "GO" then
+      rdy = false
+    else
+      rdy = true
+    end
+end
+
+step = function()
+    if I[1] == 1 then -- if instruction command is "go"
+        state.next = "GO"
+  
+      local angle = I[2] -- angle in degrees
+      local distance = I[3] -- distance in cm
+  
+      -- Here, you can calculate the control values for the left and right motors (L, R) based on the angle and distance.
+      -- For example:
+      L = V + W * angle
+      R = V - W * angle
+  
+      M = "Moving to position"
+    else
+      state.next = "WAIT"
+  
+      L = 0
+      R = 0
+  
+      M = "Stopped"
+    end
+end
+
+-- Initialize variables and state
+init()
+
+-- Function to simulate receiving new instructions; normally, you'd get this from some external source
+function receive_new_instruction(cycle)
+  if cycle % 3 == 0 then
+    I[1] = 1  -- go command
+    I[2] = 30  -- angle in degrees
+    I[3] = 200  -- distance in cm
+  elseif cycle % 3 == 1 then
+    I[1] = 1
+    I[2] = -45
+    I[3] = 100
+  else
+    I[1] = 0  -- stop command
+    I[2] = 0
+    I[3] = 0
+  end
+end
+
+-- Loop to simulate the main program
+local cycle = 0
+while cycle < 10 do -- Replace 10 with the appropriate number of cycles or a condition for exiting the loop
+  print("Cycle:", cycle)
+  
+  -- Simulate receiving new instructions
+  receive_new_instruction(cycle)
+  
+  -- Update the state and variables based on the previous state and inputs
+  forward()
+  
+  -- Update the outputs based on the current state and inputs
+  step()
+  
+  -- Print or otherwise use the outputs
+  print("Angle:", I[2], "Distance:", I[3], "L:", L, "R:", R, "Message:", M)
+  
+  cycle = cycle + 1  -- Increment the cycle counter
+end
+
+print("Program exited")
